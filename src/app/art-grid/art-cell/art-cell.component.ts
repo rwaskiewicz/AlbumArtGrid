04004dc65bab3fa565fe26cee6e5e83d5156368a
ryan.waskiewicz <ryanwaskiewicz@gmail.com>
2017-01-10 22:39:10 -0500

    Pass data to md-dialog

--- src/app/art-grid/art-cell/art-cell.component.ts
+++ src/app/art-grid/art-cell/art-cell.component.ts
@@ -13,19 +13,24 @@ export class ArtCellComponent {
   constructor(private dialog: MdDialog) { }
 
   openAlbumDetails() {
-    this.dialog.open(CellDetailsComponent);
+    let dialogRef = this.dialog.open(CellDetailsComponent);
+    dialogRef.componentInstance.title = this.fullAlbum.name;
+    dialogRef.componentInstance.body = this.fullAlbum.artists[0].name;
   }
 }
 
 @Component({
   template: `
-    <h1 md-dialog-title>Album Title</h1>
-    <div md-dialog-content>Album Details</div>
+    <h1 md-dialog-title>Album Title: {{title}}</h1>
+    <p>Artist: {{body}}</p>
     <div md-dialog-actions>
-      <button md-button (click)="dialogRef.close()">Close</button>
+      <button md-dialog-close (click)="dialogRef.close()">Close</button>
     </div>
 `
 })
 export class CellDetailsComponent {
+  public title: string;
+  public body: string;
+
   constructor(public dialogRef: MdDialogRef<CellDetailsComponent>) { }
 }
