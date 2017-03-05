d88c0494e57c59294f72f6226b70a3f58924b432
ryan.waskiewicz <ryanwaskiewicz@gmail.com>
2017-03-05 13:47:56 -0500

    Slow down flipping, clean up timing svc

--- src/app/timing/timing.service.ts
+++ src/app/timing/timing.service.ts
@@ -6,20 +6,17 @@ import { WindowService} from '../window/window.service';
 @Injectable()
 export class TimingService {
   private upperBounds: number;
-  private emitIntervalInMilliseconds: number = 1000;
+  private emitIntervalInMilliseconds: number = 2000;
 
   constructor(private windowService: WindowService) {
     this.upperBounds = windowService.calculateTotalNumberOfCells();
   }
 
-  // TODO: Bug wherein there are this.upperBounds running services at once
   emitRandomIndex(): Observable<number> {
     return Observable
       .interval(this.emitIntervalInMilliseconds)
       .map(() => {
-        let rand = Math.floor(Math.random() * this.upperBounds);
-        console.log(rand);
-        return rand;
+        return Math.floor(Math.random() * this.upperBounds);
       });
   }
 }
