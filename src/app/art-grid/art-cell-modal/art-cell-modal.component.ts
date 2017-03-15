ea4856b01d00b8a73ab46ac27117713ef09957e1
ryan.waskiewicz <ryanwaskiewicz@gmail.com>
2017-03-14 21:13:20 -0400

    Add Bootstrap, begin mocking out layout of modal

--- src/app/art-grid/art-cell-modal/art-cell-modal.component.ts
+++ src/app/art-grid/art-cell-modal/art-cell-modal.component.ts
@@ -4,7 +4,7 @@ import { MdDialogRef } from '@angular/material';
 import { AlbumFull } from '../../dto/album-full';
 
 @Component({
-  styleUrls: ['./art-cell-modal.component.css'],
+  styleUrls: ['./art-cell-modal.component.css', '../../../../node_modules/bootstrap/dist/css/bootstrap.min.css'],
   templateUrl: './art-cell-modal.component.html'
 })
 export class ArtCellModalComponent {
